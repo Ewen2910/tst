33e6f5be553b3fde5b46a0ead2d823f51573db8d
Ewen2910 <ewen1.malenfant@epitech.eu>
2024-09-25 12:29:21 +0200

    add and pipeline creation

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+def add(nbr,nbrt) -> int:
+
+    return nbr+nbrt
+
+def main() -> int:
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main()) 
---
+++ test_main.py
@@ -0,0 +1,14 @@
+from main import add
+
+
+def test_add1n1():
+    assert add(1,1) == 2
+
+def test_add3n1():
+    assert add(3,1) == 4
+
+def test_addm3n3():
+    assert add(-3,3) == 0
+
+def test_addm3nm3():
+    assert add(-3,-3) == -6
